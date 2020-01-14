b2975636ebecfb9cb90cd7fe2de7749d7cca8048
Sean Goldsborough <smgoldsborough@gmail.com>
2020-01-13 19:18:20 -0500

    refactor: removes comments from tests

--- NASAPhotosUITests/NASAPhotosUITests.swift
+++ NASAPhotosUITests/NASAPhotosUITests.swift
@@ -12,13 +12,8 @@ var app: XCUIApplication!
 class NASAPhotosUITests: XCTestCase {
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         app = XCUIApplication()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDown() {
@@ -26,7 +21,6 @@ class NASAPhotosUITests: XCTestCase {
     }
 
     func testNavBarTitle() {
-        // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
         
@@ -38,7 +32,6 @@ class NASAPhotosUITests: XCTestCase {
 
     func testLaunchPerformance() {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
             measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
                 XCUIApplication().launch()
             }
