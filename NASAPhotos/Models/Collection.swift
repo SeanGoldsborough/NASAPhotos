dd142ed16b077a62a9a26bdcda095a25159c2ba6
Sean Goldsborough <smgoldsborough@gmail.com>
2020-01-13 19:28:12 -0500

    refactor: removes unused code from collection model

--- NASAPhotos/Models/Collection.swift
+++ NASAPhotos/Models/Collection.swift
@@ -8,28 +8,8 @@
 
 import Foundation
 struct PhotoCollection : Codable, Hashable {
-    //let links : [Links]?
+    let links : [Links]?
     let version : String?
     let items : [Items]?
     let href : String?
-   // let metadata : Metadata?
-
-    enum CodingKeys: String, CodingKey {
-
-       // case links = "links"
-        case version = "version"
-        case items = "items"
-        case href = "href"
-      //  case metadata = "metadata"
-    }
-
-    init(from decoder: Decoder) throws {
-        let values = try decoder.container(keyedBy: CodingKeys.self)
-       // links = try values.decodeIfPresent([Links].self, forKey: .links)
-        version = try values.decodeIfPresent(String.self, forKey: .version)
-        items = try values.decodeIfPresent([Items].self, forKey: .items)
-        href = try values.decodeIfPresent(String.self, forKey: .href)
-       // metadata = try values.decodeIfPresent(Metadata.self, forKey: .metadata)
-    }
-
 }
